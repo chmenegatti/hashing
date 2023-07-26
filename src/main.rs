f6338d7f481e6e7b054e26d61aaf5a8c83696a91
Cesar Oliveira <cesar.oliveira@totvs.com.br>
2023-07-26 14:14:46 -0300

    📦 chore: add .gitignore, Cargo.toml, and src/main.rs files 🔧 fix(.gitignore): add /target directory to ignore list 🔧 fix(Cargo.toml): add missing newline at the end of the file ✨ feat(src/main.rs): implement password hashing functionality Added the .gitignore file to ignore the /target directory. The Cargo.toml file was created with the necessary package information and dependencies. The src/main.rs file was added and contains the implementation of password hashing functionality using MD5, SHA256, and SHA512 algorithms.

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::io;
+use crypto::digest::Digest;
+use crypto::md5::Md5;
+use crypto::sha2::{Sha256, Sha512};
+
+fn main() {
+    loop {
+        println!("Digite a senha (ou 'quit' para sair):");
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Falha ao ler a entrada");
+        let password = input.trim();
+
+        if password == "quit" || password == "exit" {
+            println!("Saindo da aplicação.");
+            break;
+        }
+
+        println!("MD5: {}", hash_md5(password));
+        println!("SHA256: {}", hash_sha256(password));
+        println!("SHA512: {}", hash_sha512(password));
+    }
+}
+
+fn hash_md5(password: &str) -> String {
+    let mut hasher = Md5::new();
+    hasher.input_str(password);
+    hasher.result_str()
+}
+
+fn hash_sha256(password: &str) -> String {
+    let mut hasher = Sha256::new();
+    hasher.input_str(password);
+    hasher.result_str()
+}
+
+fn hash_sha512(password: &str) -> String {
+    let mut hasher = Sha512::new();
+    hasher.input_str(password);
+    hasher.result_str()
+}
